274105a2bf2871dc106f1f30647fa6ee03d1ca04
mark <mark123@hotmail.com>
2021-08-02 14:03:33 +1000

    Configure project
    Install bootstrap 5 & angular-eslint
    Create modules: core, shared & home
    
    Modify configuration files: package.json, angular.json & tsconfig.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Navigation } from '@core/model/navigation.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'project';
+export class AppComponent implements OnInit {
+	public items: Navigation[] = [];
+
+	ngOnInit() {
+		this.items.push(new Navigation('Home', '/home'));
+	}
 }
---
+++ src/app/core/model/navigation.model.ts
@@ -0,0 +1,4 @@
+export class Navigation {
+	constructor(public label: string,
+		public url: string) {}
+}
